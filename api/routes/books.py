93b98f6169cab7ca491b922da808e5454eaa174d
Danielle Badobre <badobredanielle@gmail.com>
2025-02-11 23:10:07 +0000

    test: bad code pull request

--- api/routes/books.py
+++ api/routes/books.py
@@ -1,9 +1,3 @@
-from typing import OrderedDict
-
-from fastapi import APIRouter, status
-from fastapi.responses import JSONResponse
-
-from api.db.schemas import Book, Genre, InMemoryDB
 
 router = APIRouter()
 
